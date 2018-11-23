6936815e49a29b5d15d758513a8cd6fbbce883fb
Gaston Yelmini <gastonyelmini@gmail.com>
2018-11-22 21:57:46 -0300

    New dni data

---
+++ database/migrations/2018_11_23_005537_new_dnis_data.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NewDnisData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('dnis', function($table) {
+            $table->string('name_lastname');
+            $table->string('address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dnis', function($table) {
+            $table->dropColumn('name_lastname');
+            $table->dropColumn('address');
+        });
+    }
+}
